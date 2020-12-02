8f57c213028c99b210a618b3e7cb7a3dcde46801
Clara <>
2020-12-02 15:22:49 +0100

    formulario Edit

--- back_proyecto/models/book.js
+++ back_proyecto/models/book.js
@@ -1,7 +1,7 @@
 
 const getAll = () => {
     return new Promise( (resolve, reject) =>{
-        db.query( 'SELECT * FROM clientes', (error, rows) => {
+        db.query( 'SELECT servicios.fecha, servicios.hora_inicio, mesas.numero as "mesa", CONCAT(clientes.nombre, " ", clientes.apellidos) as "nombre", clientes.telefono FROM restaurante.servicios, restaurante.mesas, restaurante.clientes WHERE mesas.id = servicios.fk_mesa AND clientes.id = servicios.fk_cliente;', (error, rows) => {
             if(error) reject(error);
             resolve(rows);
         });
