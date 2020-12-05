97d3af09740251ad0d4a88ceaa26087f204d098c
Clara <>
2020-12-05 13:18:24 +0100

    edit

--- back_proyecto/models/book.js
+++ back_proyecto/models/book.js
@@ -1,15 +1,15 @@
 
-const getAll = () => {
-    return new Promise( (resolve, reject) =>{
-        db.query( 'SELECT servicios.fecha, servicios.hora_inicio, servicios.pax, CONCAT(clientes.nombre, " ", clientes.apellidos) as "nombre", clientes.telefono FROM restaurante.servicios, restaurante.clientes WHERE servicios.fk_cliente = clientes.id', (error, rows) => {
-            if(error) reject(error);
-            resolve(rows);
-            const reserva = rows;
-            console.log(reserva);
-        });
-    }); 
+// const getAll = () => {
+//     return new Promise( (resolve, reject) =>{
+//         db.query( 'SELECT servicios.fecha, servicios.hora_inicio, servicios.pax, CONCAT(clientes.nombre, " ", clientes.apellidos) as "nombre", clientes.telefono FROM restaurante.servicios, restaurante.clientes WHERE servicios.fk_cliente = clientes.id', (error, rows) => {
+//             if(error) reject(error);
+//             resolve(rows);
+//             const reserva = rows;
+//             console.log(reserva);
+//         });
+//     }); 
     
-};
+// };
 
 const getByFecha = ( pFecha ) => {
     return new Promise( (resolve, reject) => {
@@ -20,13 +20,22 @@ const getByFecha = ( pFecha ) => {
     });
 }
 
-const getMesasOfReserva = ( pReservaId ) => {
+const getMesasOfServicio = ( pServicioId ) => {
     return new Promise( (resolve, reject) => {
-        db.query( 'SELECT m.numero FROM tbi_servicios_mesas tbi, restaurante.mesas m WHERE tbi.fk_mesas = m.id AND tbi.fk_servicio = ?;', [pReservaId], (error, rows) => {
+        db.query( 'SELECT m.numero FROM tbi_servicios_mesas tbi, restaurante.mesas m WHERE tbi.fk_mesas = m.id AND tbi.fk_servicio = ?;', [pServicioId], (error, rows) => {
             if(error) reject(error);
             resolve(rows);
         });
     });
 }
 
-module.exports = { getAll, getByFecha, getMesasOfReserva };
+const getReservaById = ( pServicioId ) => {
+    return new Promise( (resolve, reject) => {
+        db.query('SELECT s.fecha, s.hora_inicio, s.pax, s.notas, c.nombre, c.apellidos, c.telefono, c.email FROM restaurante.servicios s, restaurante.clientes c WHERE s.fk_cliente = c.id AND s.id = ?;', [pServicioId], (error, row) => {
+            if(error) reject(error);
+            resolve(row[0]);
+        });
+    });
+}
+
+module.exports = { getByFecha, getMesasOfServicio, getReservaById };
--- back_proyecto/routes/api/book.js
+++ back_proyecto/routes/api/book.js
@@ -1,18 +1,57 @@
 const router = require('express').Router();
-const { getAll, getByFecha, getMesasOfReserva } = require('../../models/book');
+const { Router } = require('express');
+const { getByFecha, getMesasOfServicio, getReservaById } = require('../../models/book');
 
+
+
+//* RECUPERA los datos de todas las reservas por fecha
 router.get('/', async (req, res) => {
     try{
-        const servicios = await getByFecha( '2020-11-30' );
-        for(let servicio of servicios){
-            servicio.mesas = await getMesasOfReserva(servicio.id)
-            console.log(servicio.id);
-            console.log( servicio );
+        const reservas = await getByFecha( '2020-11-30' );
+        for(let reserva of reservas){
+            reserva.mesas = await getMesasOfServicio(reserva.id)
+            console.log(reservas.id);
+            console.log( reservas );
         }
-        res.json(servicios);
+        res.json(reservas);
         //console.log(rows);
     }catch (error){  res.json({ error: error.message });  }
     
 });
+
+
+
+//* EDITAR RESERVA
+//* Recupera los datos de una única reserva por su id
+router.get('/edit/:idservicio', async (req, res) => {
+    try{
+        //console.log(req.params);
+        const reserva = await getReservaById( req.params.idservicio );
+        reserva.mesas = await getMesasOfServicio( req.params.idservicio );
+        reserva.id = req.params.idservicio;
+        console.log(reserva);
+        //res.json( reserva );
+        res.render('reservas/edit', {reserva});
+
+    }catch(error){ res.json({ error: error.message }) }
+});
+//* Envía los datos editados de la reserva
+router.put('/edit/:idservicio', async (req, res) => {
+    try{
+        const reserva = await getReservaById( req.params.idservicio );
+        if(result.affectedRows === 1){
+            const reservaActualizada = await getReservaById(req.body.reserva.id);
+            res.json({
+                mensaje: 'La reserva se ha actualizado',
+                reservaActualizada
+            });
+        }else {  res.json({error: 'No se ha podido actualizar la reserva'})  }
+        res.json(result);
+    }catch (error){ 
+        res.json({ error: error.message }); 
+    }
+});
+
+
 
 module.exports = router;
