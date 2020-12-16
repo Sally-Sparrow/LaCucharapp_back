8ebf015a4748e0e97c44a883229ebb0655df6a12
Clara <>
2020-12-16 16:13:00 +0100

    id reservas para formulario edit

--- back_proyecto/models/book.js
+++ back_proyecto/models/book.js
@@ -1,7 +1,7 @@
 // Devuelve las reservas con sus mesas por fecha (+ idCliente)
 const getReservasByFecha = (pFecha) => {
     return new Promise((resolve, reject) => {
-        db.query('SELECT reservas.fecha, reservas.hora_inicio, reservas.fk_cliente as idCliente, reservas.pax, reservas.notas, GROUP_CONCAT(mesas.numero) as mesas FROM restaurante.reservas INNER JOIN restaurante.tbi_reservas_mesas ON reservas.id = tbi_reservas_mesas.fk_reservas INNER JOIN restaurante.mesas ON mesas.id = tbi_reservas_mesas.fk_mesas WHERE reservas.fecha = ? GROUP BY reservas.id', [pFecha], (error, result) => {
+        db.query('SELECT reservas.id, reservas.fecha, reservas.hora_inicio, reservas.fk_cliente as idCliente, reservas.pax, reservas.notas, GROUP_CONCAT(mesas.numero) as mesas FROM restaurante.reservas INNER JOIN restaurante.tbi_reservas_mesas ON reservas.id = tbi_reservas_mesas.fk_reservas INNER JOIN restaurante.mesas ON mesas.id = tbi_reservas_mesas.fk_mesas WHERE reservas.fecha = ? GROUP BY reservas.id', [pFecha], (error, result) => {
             if (error) reject(error);
             resolve(result);
         });
